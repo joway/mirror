d49fe096d4418c07c9d50ebd8e5bd02faf99577a
Joway Wang <joway.w@gmail.com>
2017-06-02 14:17:22 +0800

    update

--- influxdata/scripts/install.sh
+++ influxdata/scripts/install.sh
@@ -1,9 +1,24 @@
 role=${1:-general}
 
 url_prefix='http://m.sh.mk/influxdata'
+OS_ID=`awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }'`
+echo "os is $OS_ID"
 
-# wget "{$url_prefix}/package/telegraf-1.3.0-1.x86_64.rpm"
-# sudo yum localinstall telegraf-1.3.0-1.x86_64.rpm
-# rm telegraf-1.3.0-1.x86_64.rpm
+case $OS_ID in
+amzn)
+	wget "$url_prefix/package/telegraf-1.3.0-1.x86_64.rpm"
+	sudo yum localinstall telegraf-1.3.0-1.x86_64.rpm
+	rm telegraf-1.3.0-1.x86_64.rpm
+    ;;
+ubuntu)
+	wget "$url_prefix/package/telegraf-1.3.0-1_amd64.deb"
+	sudo dpkg -i telegraf_1.3.0-1_amd64.deb
+	rm telegraf_1.3.0-1_amd64.deb
+    ;;
+*)
+	ehco "your os not support"
+    exit -1
+	;;
+esac
 
-curl "{$url_prefix}/conf/telegraf.conf" | sed s/role = "general"/role = $role/
+curl "{$url_prefix}/conf/telegraf.conf" | sed s/'role = "general"'/"role = "\"${role}\"""/ > /etc/telegraf/telegraf.conf
