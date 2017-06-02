08901ab0edf4ae8431c3233ec5b50f8f77a79791
Joway Wang <joway.w@gmail.com>
2017-06-02 14:34:47 +0800

    update

--- influxdata/scripts/telegraf-install.sh
+++ influxdata/scripts/telegraf-install.sh
@@ -1,24 +1,26 @@
 role=${1:-general}
 
 url_prefix='http://m.sh.mk/influxdata'
-OS_ID=`awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }'`
+OS_ID=`awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' | tr -d '"'`
 echo "os is $OS_ID"
 
 case $OS_ID in
 amzn)
 	wget "$url_prefix/package/telegraf-1.3.0-1.x86_64.rpm"
-	sudo yum localinstall telegraf-1.3.0-1.x86_64.rpm
+	sudo yum localinstall telegraf-1.3.0-1.x86_64.rpm -y
 	rm telegraf-1.3.0-1.x86_64.rpm
     ;;
 ubuntu)
-	wget "$url_prefix/package/telegraf-1.3.0-1_amd64.deb"
-	sudo dpkg -i telegraf_1.3.0-1_amd64.deb
+	wget "$url_prefix/package/telegraf_1.3.0-1_amd64.deb"
+	sudo dpkg -i telegraf_1.3.0-1_amd64.deb -y
 	rm telegraf_1.3.0-1_amd64.deb
     ;;
 *)
-	ehco "your os not support"
+	echo "your os not support"
     exit -1
 	;;
 esac
 
 curl "{$url_prefix}/conf/telegraf.conf" | sed s/'role = "general"'/"role = "\"${role}\"""/ > /etc/telegraf/telegraf.conf
+
+service telegraf restart
